5b6b1cd25af01461aedbcac66cccb994a5e19b11
baosphoa0403 <baosphoa0403@gmail.com>
2022-01-31 12:28:56 +0700

    crud

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,25 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { TaskModule } from './task/task.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    UserModule,
+    TaskModule,
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'bjstlnhqypjyzutx8ypf-mysql.services.clever-cloud.com',
+      port: 3306,
+      username: 'uv0eoadwhb4s4mbm',
+      password: '6V1em5nAMg9VBSN7HU9t',
+      database: 'bjstlnhqypjyzutx8ypf',
+      entities: [],
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
